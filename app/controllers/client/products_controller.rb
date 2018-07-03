57908f257508af52d4de959e8d1cdb5efcf2d42d
Andrew Basner <andrewbasner@gmail.com>
2018-07-02 22:20:29 -0400

    make index and show methods with links to each other

---
+++ app/controllers/client/products_controller.rb
@@ -0,0 +1,19 @@
+class Client::ProductsController < ApplicationController
+
+  def index
+    response = Unirest.get('http://localhost:3000/api/products')
+    @products = response.body
+    render 'index.html.erb'
+  end
+
+  def show
+    product_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/products/#{product_id}")
+    @product1 = response.body
+    render 'show.html.erb'
+  end
+
+  def new
+    render 'new.html.erb'
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :client do
+    get '/products/' => 'products#index'
+    get '/products/new' => 'products#new'
+    get '/products/:id' => 'products#show'
+  end
+end
